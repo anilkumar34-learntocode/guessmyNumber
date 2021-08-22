253fe15631a9c4001abe6d5d7439458e74280204
anilkumar34-learntocode <80689436+anilkumar34-learntocode@users.noreply.github.com>
2021-08-22 08:47:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,48 +4,49 @@ let random = Math.floor(Math.random() * 50) + 1;
 let score = 10;
 let highScore = 0;
 let myScore = 0;
+console.log(score, highScore, myScore);
 document.querySelector('.click').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   if (guess === random) {
-    if (score > highScore && score === 10) {
-      highScore = score * 5 + 700;
-      myScore = highScore;
-    } else if (score > highScore && score === 9) {
-      highScore = score * 5 + 600;
-      myScore = highScore;
-    } else if (score > highScore && score === 8) {
-      highScore = score * 5 + 450;
-      myScore = highScore;
-    } else if (score > highScore && score === 7) {
-      highScore = score * 5 + 350;
-      myScore = highScore;
-    } else if (score > highScore && score === 6) {
-      highScore = score * 5 + 200;
-      myScore = highScore;
-    } else if (score > highScore && score === 5) {
-      highScore = score * 5 + 150;
-      myScore = highScore;
-    } else if (score > highScore && score === 4) {
-      highScore = score * 5 + 100;
-      myScore = highScore;
-      myScore = highScore;
-    } else if (score > highScore && score === 3) {
-      highScore = score * 5 + 75;
-      myScore = highScore;
-    } else if (score > highScore && score === 2) {
-      highScore = score * 5 + 50;
-      myScore = highScore;
-    } else if (score > highScore && score === 1) {
-      highScore = score * 5 + 25;
-      myScore = highScore;
+    if (score === 10) {
+      myScore = score * 5 + 700;
+      // highScore = myScore;
+    } else if (score === 9) {
+      myScore = score * 5 + 600;
+      // highScore = myScore;
+    } else if (score === 8) {
+      myScore = score * 5 + 450;
+      // highScore = myScore;
+    } else if (score === 7) {
+      myScore = score * 5 + 350;
+      // highScore = myScore;
+    } else if (score === 6) {
+      myScore = score * 5 + 200;
+      // highScore = myScore;
+    } else if (score === 5) {
+      myScore = score * 5 + 150;
+      // highScore = myScore;
+    } else if (score === 4) {
+      myScore = score * 5 + 100;
+      // highScore = myScore;
+    } else if (score === 3) {
+      myScore = score * 5 + 75;
+      // highScore = myScore;
+    } else if (score === 2) {
+      myScore = score * 5 + 50;
+      // highScore = myScore;
+    } else if (score === 1) {
+      myScore = score * 5 + 25;
+      // highScore = myScore;
     }
     document.querySelector('.number').innerHTML = random;
     document.querySelector('.myscore').innerHTML = myScore;
     document.querySelector('.message').innerHTML = `CONGRATS YOU WON`;
     document.querySelector('.h1').innerHTML = `YOU GUESSED IT RIGHT👍`;
-    document.querySelector('.highscore').innerHTML = highScore;
+    // document.querySelector('.highscore').innerHTML = highScore;
     document.querySelector('.display').style.backgroundImage =
       "url('dance.gif')";
+    // console.log(score, highScore, myScore);
   } else if (score === 1) {
     document.querySelector('.score').innerHTML = 0;
     document.querySelector('.message').innerHTML = `GAME OVER YOU LOST`;
@@ -62,32 +63,24 @@ document.querySelector('.click').addEventListener('click', function () {
     document.querySelector('.message').innerHTML = `YOUR GUESS IS LOW`;
   }
 });
+
 document.querySelector('.btn').addEventListener('click', function () {
   random = Math.floor(Math.random() * 50) + 1;
-  score = 10;
-  highScore = 0;
-  myScore = 0;
-  if (score > highScore && score === 10) {
-    highScore = score * 5 + 700;
-  } else if (score > highScore && score === 9) {
-    highScore = score * 5 + 600;
-  } else if (score > highScore && score === 8) {
-    highScore = score * 5 + 450;
-  } else if (score > highScore && score === 7) {
-    highScore = score * 5 + 350;
-  } else if (score > highScore && score === 6) {
-    highScore = score * 5 + 200;
-  } else if (score > highScore && score === 5) {
-    highScore = score * 5 + 150;
-  } else if (score > highScore && score === 4) {
-    highScore = score * 5 + 100;
-  } else if (score > highScore && score === 3) {
-    highScore = score * 5 + 75;
-  } else if (score > highScore && score === 2) {
-    highScore = score * 5 + 50;
-  } else if (score > highScore && score === 1) {
-    highScore = score * 5 + 25;
+  // console.log(score, highScore, myScore);
+  if (myScore > highScore) {
+    document.querySelector('.highscore').innerHTML = myScore;
+    highScore = myScore;
+  } else if (myScore <= highScore) {
+    document.querySelector('.highscore').innerHTML = highScore;
   }
+  // } else if (myScore === highScore) {
+  //   document.querySelector('.highscore').innerHTML = myScore;
+  // }
+
+  // console.log(score, highScore, myScore);
+
+  myScore = 0;
+  score = 10;
   document.querySelector('.myscore').innerHTML = myScore;
   document.querySelector('.guess').value = '';
   document.querySelector('.number').innerHTML = '?';
@@ -96,4 +89,6 @@ document.querySelector('.btn').addEventListener('click', function () {
   document.querySelector('.h1').innerHTML = 'Guess My Number!';
   document.querySelector('.display').style.backgroundImage =
     "url('penguin.png')";
+  // console.log(score, highScore, myScore);
 });
+// console.log(score, highScore, myScore);
